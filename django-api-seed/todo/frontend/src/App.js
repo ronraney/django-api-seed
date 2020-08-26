13e7adbc99934deee54ca5d821193d705316c21e
Ron Raney <raneyron@yahoo.com>
2020-08-25 21:34:24 -0500

    First commit: React/Django API

---
+++ django-api-seed/todo/frontend/src/App.js
@@ -0,0 +1,37 @@
+// App.js
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    todos: []
+  };
+/* 
+   This is where the magic happens
+*/
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/'); // fetching the data from api, before the page loaded
+      const todos = await res.json();
+      this.setState({
+        todos
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.todos.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.description}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
